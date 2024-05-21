d448f61d7f1cc1d5348383a21170bac881811e17
erfan <erfanfarhanian7739@gmail.com>
2024-05-21 13:22:58 +0330

    Add ProductAPI.Core, Product entity and IProductRepository interface

---
+++ ProductAPI.Core/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the name of product.")]
+        [MaxLength(150, ErrorMessage = "The value entered is greater than the limit.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the produce date.")]
+        public DateTime ProduceDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter the manufacture phone.")]
+        [MaxLength(15, ErrorMessage = "The value entered is greater than the limit.")]
+        public string ManufacturePhone { get; set; }
+
+        [Required(ErrorMessage = "Please enter the manufacture Email")]
+        [EmailAddress(ErrorMessage = "Please enter the correct format of email.")]
+        [MaxLength(250, ErrorMessage = "The value entered is greater than the limit.")]
+        public string ManufactureEmail { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ ProductAPI.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(Product product);
+    }
+}
