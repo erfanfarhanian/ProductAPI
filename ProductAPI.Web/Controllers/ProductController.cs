03d725f34f3b7eb5c748eddea45a9545ae7b8c68
erfan <erfanfarhanian7739@gmail.com>
2024-05-21 13:29:00 +0330

    add ProductAPI.Web, Product and Authentication Controllers to implement required tasks and add required services in Program.cs

---
+++ ProductAPI.Web/Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Application.Products.Commands;
+using ProductAPI.Application.Products.Queries;
+using ProductAPI.Core.Entities;
+using ProductAPI.Infrastructure.Data;
+using System.Security.Claims;
+
+namespace ProductAPI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator, ApplicationDbContext context)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts([FromQuery] string? createdBy)
+        {
+            var query = new GetProductsQuery
+            {
+                CreatedBy = createdBy
+            };
+            var product = await _mediator.Send(query);
+
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var query = new GetProductByIdQuery
+            {
+                Id = id
+            };
+
+            var product = await _mediator.Send(query);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateProduct(CreateProductCommand command)
+        {
+            //command.CreatedBy = User.Identity.Name;
+            //var productId = await _mediator.Send(command);
+
+            //return CreatedAtAction(nameof(GetProduct), new { id = productId }, productId);
+
+            var userNameClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            if (string.IsNullOrEmpty(userNameClaim))
+            {
+                return BadRequest("User is not authenticated");
+            }
+
+            command.CreatedBy = userNameClaim;
+            var productId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetProduct), new { id = productId }, productId);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(int id, UpdateProductCommand command)
+        {
+            //if (id != command.Id)
+            //{
+            //    return BadRequest("You do not have permission to update this product.");
+            //}
+
+            var userNameClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            if (string.IsNullOrEmpty(userNameClaim))
+            {
+                return BadRequest("User is not authenticated");
+            }
+
+            command.Id = id;
+            command.CreatedBy = userNameClaim;
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var userNameClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            if (string.IsNullOrEmpty(userNameClaim))
+            {
+                return BadRequest("User is not authenticated");
+            }
+
+            var command = new DeleteProductCommand
+            {
+                Id = id,
+                CreatedBy = userNameClaim
+            };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ ProductAPI.Web/Program.cs
@@ -0,0 +1,110 @@
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ProductAPI.Application.Common.Mappings;
+using ProductAPI.Application.Products.Commands;
+using ProductAPI.Core.Interfaces;
+using ProductAPI.Infrastructure.Data;
+using ProductAPI.Infrastructure.Repositories.Implementation;
+using System.Reflection;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+// Add Repositories and MediatR
+builder.Services.AddMediatR(typeof(CreateProductCommand).Assembly);
+builder.Services.AddMediatR(typeof(UpdateProductCommand).Assembly);
+builder.Services.AddMediatR(typeof(DeleteProductCommand).Assembly);
+//builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddControllers();
+
+// Add Mappers
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductAPI v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
